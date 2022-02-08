d29454b1768653f8ea3ba224f2f4bea0157ee295
Rahaf Hosari <74103723+Rahafhosari@users.noreply.github.com>
2022-02-08 15:03:06 +0200

    added files

---
+++ Button Clicker/ButtonClicker.js
@@ -0,0 +1,16 @@
+function logout(button) {
+    button.innerText = "Logout";
+}
+
+// function alert(element) {
+//     "Ninja was liked";
+//     console.log("Ninja was liked", element);
+// }
+
+function hide(element) {
+    element.remove();
+}
+
+function popUp(element) {
+    alert('Ninja was liked');
+}
